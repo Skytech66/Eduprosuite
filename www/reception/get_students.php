bb7604f7b72d21f34b6d833b7eae075f7be8ab37
Skytech66 <dadzieitservices123@gmail.com>
2025-07-17 09:58:39 +0000

    Delete www/reception directory

--- www/reception/get_students.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pdo = new PDO("sqlite:students_records.db");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$stmt = $pdo->query("SELECT * FROM students");
-$students = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($students);
-?>
