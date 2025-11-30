8f52f52af2504b229c00b2f2be0e43dca83539f9
Skytech66 <dadzieitservices123@gmail.com>
2025-11-30 20:11:04 +0000

    Update smart-404.php

--- smart-404.php
+++ smart-404.php
@@ -1,8 +1,6 @@
 <?php
-// Get the originally requested URL
 $requestedUrl = $_SERVER['REQUEST_URI'];
 
-// Output a page that first tries to reload the requested URL
 echo '<!DOCTYPE html>
 <html>
 <head>
@@ -22,7 +20,6 @@ body {
 <p>If the page does not load, you will be redirected shortly.</p>
 
 <script>
-// After 5 seconds, redirect to the correct dashboard location
 setTimeout(function(){
     window.location.href = "/www/teachers/dashboard.php";
 }, 5000);
